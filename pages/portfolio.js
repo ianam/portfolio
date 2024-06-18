81ae7e26815a5625d17bd1fa661551f6c7ff3833
Iana Markevitch <Babbu@m2dimo.local>
2024-06-18 10:18:19 -0700

    Tailwind CSS working

---
+++ pages/portfolio.js
@@ -0,0 +1,10 @@
+function Portfolio() {
+    return (
+        <div className="bg-gray-100 p-8">
+            <h1 className="text-2xl font-bold text-gray-800">Portfolio Page</h1>
+            <p className="text-orange-500">This is a paragraph.</p>
+        </div>
+    )
+}
+
+export default Portfolio;
