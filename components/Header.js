9bcb694b373fac04086afdbaed2a96ee086f1f8e
Iana Markevitch <iana.markevitch@gmail.com>
2024-06-18 14:02:20 -0700

    Add Header

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <header>
+            <h1>Iana's Portfolio</h1>
+            <nav>
+                <a href="/">Home</a>
+                <a href="/">Projects</a>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
