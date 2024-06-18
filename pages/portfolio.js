646b83bc699d7245fed69e263549b4f32c159841
Iana Markevitch <Babbu@m2dimo.local>
2024-06-18 14:02:20 -0700

    Add Header

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,9 +1,14 @@
+import Header from '../components/Header';
+
 function Portfolio() {
     return (
-        <div className="bg-gray-100 p-8">
-            <h1 className="text-2xl font-bold text-gray-800">Portfolio Page</h1>
-            <p className="text-orange-500">This is a paragraph.</p>
-        </div>
+        <div>
+            <Header />
+            <div className="bg-gray-100 p-8">
+                <h1 className="text-2xl font-bold text-gray-800">Portfolio Page</h1>
+                <p className="text-orange-500">This is a paragraph.</p>
+            </div>
+        </div>        
     )
 }
 
