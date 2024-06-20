3d4d4fc95806c4a424cee0abdad06d4dbb6ccf32
Iana Markevitch <Babbu@m2dimo.local>
2024-06-20 09:31:29 -0700

    Nav styling

--- components/Header.js
+++ components/Header.js
@@ -3,13 +3,17 @@ import Link from 'next/link';
 const Header = () => {
     return (
         <header className="flex flex-col h-screen">
-            <nav className="bg-blue-100 w-64 min-h-full p-4 flex flex-col items-start">
-                <h1 className="text-gray-500 text-2xl font-bold mb-6">Iana Design</h1>
-                <Link href="/home" className="text-gray-500 hover:underline mb-2">Home</Link>
-                <Link href="/about" className="text-gray-500 hover:underline mb-2">About</Link>
-                <Link href="/portfolio" className="text-gray-500 hover:underline mb-2">Portfolio</Link>
-                <Link href="/services" className="text-gray-500 hover:underline mb-2">Services</Link>
-                <Link href="/contact" className="text-gray-500 hover:underline mb-2">Contact</Link>
+            <nav className="border-r-2 border-slate-100 min-h-full w-64 p-4 flex flex-col items-start justify-between">
+            <div className="flex flex-col justify-center grow">
+                <Link href="/home" className="text-lg uppercase text-gray-500 hover:underline mb-4">Home</Link>
+                <Link href="/about" className="text-lg uppercase text-gray-500 hover:underline mb-4">About</Link>
+                <Link href="/portfolio" className="text-lg uppercase text-gray-500 hover:underline mb-4">Portfolio</Link>
+                <Link href="/services" className="text-lg uppercase text-gray-500 hover:underline mb-4">Services</Link>
+                <Link href="/contact" className="text-lg uppercase text-gray-500 hover:underline mb-4">Contact</Link>                    
+            </div>
+            <div>
+                    <p className="text-sm text-gray-400">Copyright 2024</p>
+                </div>
             </nav>
         </header>
     );
