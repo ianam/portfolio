167c2227f802314429bf693bcbf1e728cce3f242
Iana Markevitch <iana.markevitch@gmail.com>
2024-06-20 09:31:29 -0700

    Nav styling

--- pages/home.js
+++ pages/home.js
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <div className="flex">
             <Header />
-            <div className="bg-gray-100 p-4 w-screen">
+            <div className="p-4 w-screen">
                 <h1 className="text-2xl font-bold text-gray-800">Home Page</h1>
             </div>
         </div>        
