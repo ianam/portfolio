077b8ec399cc1e44ab33ce16972475d07de98342
Iana Markevitch <iana.markevitch@gmail.com>
2024-06-21 10:19:26 -0700

    Define page transition CSS

---
+++ components/AnimatedHeading.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+const AnimatedHeading = ({ text }) => {
+    const [displayedText, setDisplayedText] = useState('');
+};
+
+export default AnimatedHeading;
