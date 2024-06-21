981200639710a52b7a7ff5bd79b06dc1711b35ca
Iana Markevitch <iana.markevitch@gmail.com>
2024-06-21 10:08:26 -0700

    Add bio & remove About page

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,6 @@ const Header = () => {
             <nav className="border-r-2 border-slate-100 min-h-full w-64 p-4 flex flex-col items-start justify-between">
             <div className="flex flex-col justify-center grow">
                 <Link href="/home" className="text-lg uppercase text-gray-500 mb-4 link-underline link-yellow">Home</Link>
-                <Link href="/about" className="text-lg uppercase text-gray-500 mb-4 link-underline link-orange">About</Link>
                 <Link href="/portfolio" className="text-lg uppercase text-gray-500 mb-4 link-underline link-pink">Portfolio</Link>
                 <Link href="/services" className="text-lg uppercase text-gray-500 mb-4 link-underline link-purple">Services</Link>
                 <Link href="/contact" className="text-lg uppercase text-gray-500 mb-4 link-underline link-blue">Contact</Link>                    
