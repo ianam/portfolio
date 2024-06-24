e8cbf1557167e52039b8c7337a458d49b8e0da57
Iana Markevitch <iana.markevitch@gmail.com>
2024-06-24 09:40:32 -0700

    Add image folder & profile photo

--- components/AnimatedHeading.js
+++
@@ -1,8 +0,0 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-
-const AnimatedHeading = ({ text }) => {
-    const [displayedText, setDisplayedText] = useState('');
-};
-
-export default AnimatedHeading;
--- pages/home.js
+++ pages/home.js
@@ -7,14 +7,18 @@ const Home = () => {
             <div className="p-4 w-screen">
                 <h1 className="text-8xl uppercase font-bold text-gray-200">Welcome</h1>
                 <section className="pt-10">
-                    <h2 className="text-4xl font-serif mb-4 text-[#ffbe0b]">Hi, I'm Iana</h2>
-                    <p>I'm a Vancouver-based web designer and developer, cat-lover, perfectionist, and mom! I work with local businesses to cultivate their online presence through functional and beautiful websites.</p>
-                    <h2>About Me</h2>
-                    <p>In an unexpected turn of events, I discovered a passion for web development through a position in retail management. As all businesses eventually do, we came to the conclusion that we needed a website - and I decided to give it a go myself. After several failed attempts I found my footing and grew to love the process of bringing new life to a business in the digital world. Since then I've rounded out my skills through a development bootcamp, personal for-fun projects, and a multitude of websites for local individuals and businesses.</p>
-                    <p>I pride myself on open and honest communication with clients - no technical babble (unless you're into that), and working closely with you to bring your vision to life. I want to support you in your venture - whether you need a website, graphic design, photography, copywriting, or staff training, I can help you plan your project and see it to completion.</p>
+                    <img src="public/images/IMG_4199.jpeg" />
+                    <h2 className="text-4xl font-bold mb-4 text-[#ffbe0b]">Hi, I'm <span className='text-5xl font-thin cursive'>iana</span></h2>
+                    <p>I’m a Vancouver-based web developer and designer, working with local businesses to cultivate their online presence through functional and beautiful websites. 
+                    </p>
+                    <h2 className="text-4xl font-bold my-4 text-[#ffbe0b]">About me</h2>
+                    <p>My journey into web development began when I was working for a small business, taking on the task of creating an e-commerce site for our extensive inventory. Immersing myself in the entire process - from web design, to photography, shipping logistics, copywriting, graphic design, and staff training -  I discovered a passion for project management and an itch to learn more about web development.
+                    </p>
+                    <p>Since then I’ve taught myself to code through courses and projects, and have worked with multiple clients, creating tailored web solutions for their online businesses. Creativity, problem-solving, and communication are at the core of my work, allowing me to offer comprehensive solutions that go beyond just web development. Whether you’re looking to enhance or renew your brand, expand your audience, open an e-commerce sales avenue, or create something entirely new - let’s talk about it!
+                    </p>
                 </section>
                 <section>
-                    <h3>Check out my work!</h3>
+                    <h2 className="text-4xl font-bold my-4 text-[#ffbe0b]">Check out my work!</h2>
                     <p>CV, Github, Portfolio, Services, Contact</p>
                 </section>
             </div>
